ad3b7b5e547b648cee857e22eac8db19e22bdd8b
Alberto Casero <alberto@kasfactory.net>
2018-05-29 00:23:27 +0200

    Nuestro primer hello world en Django

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ads.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', hello_world)
 ]
