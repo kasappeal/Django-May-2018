4b2d231cc6208ed55ac48040b022658677d3fae1
Alberto Casero <alberto@kasfactory.net>
2018-05-29 23:15:35 +0200

    Implementamos una primera versión del listado de anuncios en la home

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 
+from ads.models import Ad
 
-def hello_world(request):
-    return HttpResponse('Hello world!')
+
+def home(request):
+    """
+    Muestra el listado de los últimos anuncios
+    :param request: objeto HttpRequest
+    :return: HttpResponse con respuesta
+    """
+    # recuperar los anuncios de la base de datos
+    ads = Ad.objects.all()
+
+    # construir el documento HTML
+    html = '<ul>'
+    for ad in ads:
+        html += '<li>{0}</li>'.format(ad)
+    html += '</ul>'
+
+    # devolver la respuesta
+    return HttpResponse(html)
--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from ads.views import hello_world
+from ads.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', hello_world)
+    path('', home)
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
