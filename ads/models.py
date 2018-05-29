ad71f60ba37e8d8bd8e066fc468803438aeae7d0
Alberto Casero <alberto@kasfactory.net>
2018-05-29 21:39:06 +0200

    Creamos nuestro primer modelo

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+
+    BUY = 'BUY'
+    SELL = 'SEL'
+    TYPES = (
+        (BUY, 'Buying'),
+        (SELL, 'Selling')
+    )
+
+    PENDING = 'PEN'
+    APPROVED = 'APR'
+    FINISHED = 'FIN'
+    STATUSES = (
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (FINISHED, 'Finished')
+    )
+
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)  # null=True, blank=True => campo opcional
+    price = models.FloatField()
+    image = models.FileField(null=True)  # null=True => campo opcional
+    send_choice = models.BooleanField(default=False)
+    type = models.CharField(max_length=3, choices=TYPES, default=SELL)
+    status = models.CharField(max_length=3, choices=STATUSES, default=PENDING)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
