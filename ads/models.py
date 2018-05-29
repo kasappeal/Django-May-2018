1e4ba18962484e8663bb94c82e8613f478c68fcd
Alberto Casero <alberto@kasfactory.net>
2018-05-29 22:31:47 +0200

    Añadimos el método `__str__` para que en el admin de Django aparezcan correctamente los nombres de los anuncios y sus precios

--- ads/models.py
+++ ads/models.py
@@ -28,3 +28,9 @@ class Ad(models.Model):
     status = models.CharField(max_length=3, choices=STATUSES, default=PENDING)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """
+        Define cómo se representa un Ad como una string
+        """
+        return '{0} ({1} €)'.format(self.name, self.price)
