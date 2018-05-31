6edcbeb2f514d17ba9ac282224225d097225f8fd
Alberto Casero <alberto@kasfactory.net>
2018-05-31 21:15:10 +0200

    Añadimos el campo owner al modelo Ad

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -19,6 +20,7 @@ class Ad(models.Model):
         (FINISHED, 'Finished')
     )
 
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)  # null=True, blank=True => campo opcional
     price = models.FloatField()
