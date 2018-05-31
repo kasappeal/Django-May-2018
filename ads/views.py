ad7a3498df0694ae0a79537a3320251d1b3b7ba3
Alberto Casero <alberto@kasfactory.net>
2018-05-31 21:15:25 +0200

    Añadimos la vista de detalle de un anuncio

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from ads.models import Ad
@@ -17,3 +18,24 @@ def home(request):
 
     # devolver la respuesta utilizando una plantilla
     return render(request, 'ads/list.html', context)
+
+
+def ad_detail(request, pk):
+    """
+    Muestra el detalle de un anuncio
+    :param request: objeto HttpRequest
+    :param pk: identificador del anuncio
+    :return: HttpResponse con la respuesta
+    """
+    # recuperar de la base de datos el anuncio que me piden
+    try:
+        ad = Ad.objects.select_related().get(pk=pk)
+    except Ad.DoesNotExist:
+        # si no existe el anuncio, devolvemos un 404
+        return HttpResponse('No existe el anuncio que buscas', status=404)
+
+    # si existe el anuncio, creamos el contexto
+    context = {'ad': ad}
+
+    # devolver la respuesta utilizando una plantilla
+    return render(request, 'ads/detail.html', context)
--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from ads.views import home
+from ads.views import home, ad_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', home)
+    path('', home),
+    path('ad/<int:pk>', ad_detail)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
