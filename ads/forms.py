8e4caafd5fa6079d8c2f61a5f7f678411386d68b
Alberto Casero <alberto@kasfactory.net>
2018-06-04 23:13:23 +0200

    Añadimos validacion de varios campos a traves del método clean

--- ads/forms.py
+++ ads/forms.py
@@ -16,3 +16,10 @@ class AdForm(ModelForm):
         if image is not None and 'image' not in image.content_type:
             raise ValidationError('El archivo no es una imagen válida')
         return image
+
+    def clean(self):
+        super().clean()  # al llamar al método clean de la superclase garantizamos la validacion de los campos del modelo
+        send_choice = self.cleaned_data.get('send_choice', False)
+        type = self.cleaned_data.get('type')
+        if type == Ad.BUY and send_choice:
+            raise ValidationError('Los anuncios de compra no pueden llevar envío.')
