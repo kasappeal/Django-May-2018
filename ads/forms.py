2510fd5e1a99bb30ebc99a6ea73ab2753d23765f
Alberto Casero <alberto@kasfactory.net>
2018-06-04 23:05:52 +0200

    Validamos que el archivo que se sube es una imagen

--- ads/forms.py
+++ ads/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from ads.models import Ad
@@ -9,3 +10,9 @@ class AdForm(ModelForm):
         model = Ad
         fields = '__all__'
         exclude = ['owner', 'status']
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if image is not None and 'image' not in image.content_type:
+            raise ValidationError('El archivo no es una imagen válida')
+        return image
