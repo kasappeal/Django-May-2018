18a5f255e70780d7a54ac6fa865fcdc03a98db73
Alberto Casero <alberto@kasfactory.net>
2018-06-05 22:24:46 +0200

    Devolvemos correctamente los datos utilizando las herramientas que nos da REST Framework

--- users/api.py
+++ users/api.py
@@ -1,11 +1,9 @@
-import json
-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class UsersAPI(View):
+class UsersAPI(APIView):
 
     def get(self, request):
         """
@@ -21,4 +19,4 @@ class UsersAPI(View):
                 'first_name': user.first_name,
                 'last_name': user.last_name
             })
-        return HttpResponse(json.dumps(response))
+        return Response(response)
