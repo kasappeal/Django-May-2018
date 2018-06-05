918ab9fdf114722156b4d3880ac832e8080ab439
Alberto Casero <alberto@kasfactory.net>
2018-06-05 23:49:29 +0200

    Implementamos endpoint de registro de usuario

--- users/api.py
+++ users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,6 +18,19 @@ class UsersAPI(APIView):
         users = User.objects.all()
         serializer = UserListSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Crea un usuario y devuelve la información del usuario creado
+        :param request: objeto de tipo HttpRequest
+        :return:  objeto Response con datos del usuario creado o 400 con los errores cometidos
+        """
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserDetailAPI(APIView):
--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
@@ -11,3 +12,13 @@ class UserSerializer(UserListSerializer):
 
     first_name = serializers.CharField()
     last_name = serializers.CharField()
+    password = serializers.CharField()
+
+    def create(self, validated_data):
+        user = User()
+        user.username = validated_data.get('username')
+        user.first_name = validated_data.get('first_name')
+        user.last_name = validated_data.get('last_name')
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
